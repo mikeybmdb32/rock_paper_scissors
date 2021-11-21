f06c276381ed01559405d8b91b4c9ba29f43fa1c
Mike <mikemania32@hotmail.com>
2021-11-21 09:14:40 -0700

    included buttons for input and changed game end criteria to first to 5 wins

--- Rock-Paper-Scissors.js
+++ Rock-Paper-Scissors.js
@@ -1,84 +1,73 @@
 //ROCK/PAPER/SCISSORS GAME
+//first to 5 successful rounds of play wins
 
-game();
-
-//play 5 rounds of rock paper scissors
-//game winner is whoever won the most rounds
-function game(){
-    let userScore = 0;
-    let computerScore = 0;
-    for(i=1; i <= 5; i++){
-        roundWinner = determineRoundWinner();
-        if(roundWinner == "user") userScore++;
-        if(roundWinner == "computer") computerScore++;
+let userScore = 0;
+let computerScore = 0;
+let clickEvent;
+let roundwinner;
+const rpsButtons = document.querySelectorAll('.rpsButton');
+//when user clicks rock/paper/scissors buttons, play game
+rpsButtons.forEach(rpsButton => rpsButton.addEventListener('click', function(clickEvent) {;
+    if (['rock', 'paper', 'scissors'].includes(clickEvent.target.id)) {
+        roundWinner = getRoundWinner(clickEvent.target.id);
+        if (roundWinner == 'user') userScore++;
+        if (roundWinner == 'computer') computerScore++;
+        //first to 5 successful plays wins; scores are reset after winner of game is determined
+        if (userScore >= 5 || computerScore >= 5) {
+            displayGameWinner(userScore, computerScore);
+            userScore = 0;
+            computerScore = 0;
+        }
     }
+}));
 
-    displayGameWinner(userScore, computerScore);
-}
-
-
-function displayGameWinner(userScore, computerScore){
-    if(userScore > computerScore) console.log(`User wins!`);
-    if(userScore < computerScore) console.log(`Computer wins!`);
+//display winner of current round of overall game
+function displayGameWinner(userScore, computerScore) {
+    if (userScore > computerScore) console.log(`User wins!`);
+    if (userScore < computerScore) console.log(`Computer wins!`);
     else console.log(`It's a tie!`);
     console.log(`User Score:${userScore}  Computer Score:${computerScore}`);
 }
 
-function determineRoundWinner(){
-    let userPlay = window.prompt("Choose rock, paper, or scissors").toLowerCase();
-    while(userPlay != "rock" && userPlay != "paper" && userPlay != "scissors"){
-        userPlay = window.prompt("Invalid selection.  Choose rock, paper, or scissors").toLowerCase()
-    }
-    let computerPlay = computerPlayGen();
-    let winner = winnerGen(userPlay,computerPlay)
-    displayRoundWinner(userPlay,computerPlay, winner);
-    return winner;
-}
-
-function displayRoundWinner(userPlay,computerPlay, winner){
+//display winner of current round of play
+function displayRoundWinner(userPlay, computerPlay, winner) {
     console.log(`
     User Choice: ${userPlay}
     Computer Choice:  ${computerPlay}
     Round Winner:  ${winner}`);
 }
 
-//  use current time, in milliseconds, to generate random number 0-999
-function randomNumGen() {
+//use current system time, in milliseconds, to generate random number 0-999
+function getRandomNum() {
     let time = new Date();
     return time.getMilliseconds();
 }
 
 //generate computer's play
-function computerPlayGen() {
-    randomNum = randomNumGen();
-    if(randomNum <= 333) return "rock"
-    else if (randomNum > 333 && randomNum <= 666) return "paper"
-    else return "scissors"
+function getComputerPlay() {
+    randomNum = getRandomNum();
+    if (randomNum <= 333) return 'rock'
+    else if (randomNum > 333 && randomNum <= 666) return 'paper'
+    else return 'scissors'
 }
 
-
-//generate winner
-function winnerGen(userPlay, computerPlay){
-    if(userPlay == computerPlay) return "tie"
-    if(userPlay == "scissors"){
-        if(computerPlay == "paper") return "user"
-        if(computerPlay == "rock") return "computer"
+//determine winner of individual round of play
+function getRoundWinner(userPlay) {
+    let computerPlay = getComputerPlay();
+    let winner;
+    if (userPlay == computerPlay) winner = 'tie'
+    if (userPlay == 'scissors') {
+        if (computerPlay == 'paper') winner = 'user'
+        if (computerPlay == 'rock') winner = 'computer'
     }
-    if(userPlay == "paper"){
-        if(computerPlay == "rock") return "user"
-        if(computerPlay == "scissors") return "computer"
+    if (userPlay == 'paper') {
+        if (computerPlay == 'rock') winner = 'user'
+        if (computerPlay == 'scissors') winner = 'computer'
     }
-    if(userPlay == "rock"){
-        if(computerPlay == "scissors") return "user"
-        if(computerPlay == "paper") return "computer"
+    if (userPlay == 'rock') {
+        if (computerPlay == 'scissors') winner = 'user'
+        if (computerPlay == 'paper') winner = 'computer'
     }
+    displayRoundWinner(userPlay, computerPlay, winner);
+    return winner;
 }
-
-
-
-
-
-//print computer selection, user selection, and winner message
-//perform for 5 rounds
-
-
